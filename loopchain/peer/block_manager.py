406b32a2d30fe06fc8aa11895c3fe7a8f9c373a4
Hyerim Kim <hyerim.kim@theloop.co.kr>
2019-08-22 20:25:19 +0900

    [LC-516] Add logic to verify votes for a previous block.
    - Separate `_verify_common` with `verify_invoke` and others in `BlockVerifier`.

--- loopchain/peer/block_manager.py
+++ loopchain/peer/block_manager.py
@@ -25,7 +25,7 @@ import loopchain.utils as util
 from loopchain import configure as conf
 from loopchain.baseservice import TimerService, ObjectManager, Timer
 from loopchain.baseservice.aging_cache import AgingCache
-from loopchain.blockchain import BlockChain, CandidateBlocks, Epoch, BlockchainError, NID, exception
+from loopchain.blockchain import BlockChain, CandidateBlocks, Epoch, BlockchainError, NID, exception, BlockBuilder
 from loopchain.blockchain.blocks import Block, BlockVerifier, BlockSerializer
 from loopchain.blockchain.exception import ConfirmInfoInvalid, ConfirmInfoInvalidAddedBlock, TransactionOutOfTimeBound
 from loopchain.blockchain.exception import ConfirmInfoInvalidNeedBlockSync, TransactionDuplicatedHashError
@@ -780,11 +780,6 @@ class BlockManager:
         return vote
 
     def verify_confirm_info(self, unconfirmed_block: Block):
-        # TODO set below variable with right result.
-        check_unconfirmed_block_has_valid_confirm_info_for_prev_block = True
-        if not check_unconfirmed_block_has_valid_confirm_info_for_prev_block:
-            raise ConfirmInfoInvalid("Unconfirmed block has no valid confirm info for previous block")
-
         my_height = self.blockchain.block_height
         if my_height < (unconfirmed_block.header.height - 2):
             raise ConfirmInfoInvalidNeedBlockSync(f"trigger block sync in _vote my_height({my_height}), "
@@ -795,23 +790,29 @@ class BlockManager:
             raise ConfirmInfoInvalidAddedBlock(f"block is already added my_height({my_height}), "
                                                f"unconfirmed_block.header.height({unconfirmed_block.header.height})")
 
+        block_verifier = BlockVerifier.new(unconfirmed_block.header.version, self.blockchain.tx_versioner)
+        last_unconfirmed_block = self.blockchain.last_unconfirmed_block
+        prev_block = last_unconfirmed_block if last_unconfirmed_block else self.blockchain.last_block
+        try:
+            block_verifier.verify_common(
+                unconfirmed_block,
+                prev_block,
+                unconfirmed_block.header.peer_id,
+                reps_getter=self.blockchain.find_preps_addresses_by_roothash)
+        except Exception as e:
+            logging.error(e)
+            traceback.print_exc()
+            raise ConfirmInfoInvalid("Unconfirmed block has no valid confirm info for previous block")
+
     async def _vote(self, unconfirmed_block: Block):
         exc = None
+        logging.debug(f"unconfirmed_block.header({unconfirmed_block.header})")
         try:
             block_version = self.blockchain.block_versioner.get_version(unconfirmed_block.header.height)
+            block_builder = BlockBuilder.from_new(unconfirmed_block, self.blockchain.tx_versioner)
             block_verifier = BlockVerifier.new(block_version, self.blockchain.tx_versioner)
             block_verifier.invoke_func = self.blockchain.score_invoke
-            reps_getter = self.blockchain.find_preps_addresses_by_roothash
-
-            util.logger.debug(f"unconfirmed_block.header({unconfirmed_block.header})")
-
-            block_verifier.verify(unconfirmed_block,
-                                  self.blockchain.last_block,
-                                  self.blockchain,
-                                  ExternalAddress.fromhex_address(
-                                      self.blockchain.get_expected_generator(unconfirmed_block.header.peer_id)
-                                  ),
-                                  reps_getter=reps_getter)
+            block_verifier.verify_invoke(block_builder, unconfirmed_block, self.blockchain.last_block)
         except Exception as e:
             exc = e
             logging.error(e)
