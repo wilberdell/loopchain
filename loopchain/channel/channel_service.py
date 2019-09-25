8c86a319fa4b9edc2c05c69c1f95880237463b2f
Seongjun Ji <metalg0su@gmail.com>
2019-09-25 17:29:42 +0900

    [LC-573] Remove consensus member
     - Left out ChannelService.__consensus to be remove in LC-569

--- loopchain/channel/channel_service.py
+++ loopchain/channel/channel_service.py
@@ -51,7 +51,6 @@ class ChannelService:
         self.__peer_manager: PeerManager = None
         self.__broadcast_scheduler: BroadcastScheduler = None
         self.__rs_client: RestClient = None
-        self.__consensus = None
         self.__timer_service = TimerService()
         self.__node_subscriber: NodeSubscriber = None
 
